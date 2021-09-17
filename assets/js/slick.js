b50c66cfd3c6674cc836419e958b2921a471cbe0
John Angeles <JohnyAngeles25@gmail.com>
2021-09-17 16:11:13 +0800

    updated slick nav sync to the video

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -1,10 +1,29 @@
-$('.video-slider__list').slick({
+// $('.video-slider__list').slick({
+//     infinite: true,
+//     slidesToShow: 5,
+//     slidesToScroll: 5,
+//     centerMode:true,
+//     focusOnSelect:true,
+//     prevArrow: '<div class="prev_arrow"><i class="fas fa-chevron-circle-left"></i></span></div>',
+//     nextArrow: '<div class="next_arrow"><i class="fas fa-chevron-circle-right"></i></span></div>',
+// });
+
+$('.video-slider-wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.video-slider__list'
+  });
+  $('.video-slider__list').slick({
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 5,
-    centerMode:true,
-    focusOnSelect:true,
+    asNavFor: '.video-slider-wrapper',
+    dots:false,
+    centerMode: true,
+    focusOnSelect: true,
     prevArrow: '<div class="prev_arrow"><i class="fas fa-chevron-circle-left"></i></span></div>',
     nextArrow: '<div class="next_arrow"><i class="fas fa-chevron-circle-right"></i></span></div>',
-});
+  });
 
